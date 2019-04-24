3a4b2662e3b6eb0d085262aa4acb57ecd5229219
Reuben Rankin <reubenrankin@hotmail.com>
2019-04-24 02:00:03 +0000

    Restarting part 2

--- main.c
+++ main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
 int main() {
+    
+    int a, b, c;
+    
+    scanf(" %d %d %d ", &a , &b, &c);
+    printf(" Found integers %d %d %d", a, b, c);
+    
+    
 
 return 0;
 }
