178ea56a1119f648cdf0a56d8235fc7c1ac04485
Reuben Rankin <reubenrankin@hotmail.com>
2019-04-24 05:37:21 +0000

    (Irrelevant to project) Super cool array practise and array sorting practise.

--- main.c
+++ main.c
@@ -1,13 +1,46 @@
 #include <stdio.h>
 
-int main() {
-    
-    int a, b, c;
-    
-    scanf(" %d %d %d ", &a , &b, &c);
-    printf(" Found integers %d %d %d", a, b, c);
-    
+int main() 
+{
+   int i, temp, swapped;
+   int howMany = 10;
+   int goals[howMany];
    
+   for(i=0;i<howMany;i++)
+   {
+       goals[i] = ( rand()%25 )+1;
+   }
+    
+    printf("Original List\n");
+    for(i=0;i<howMany;i++)
+    {
+        printf("%d \n", goals[i]);
+    }
+    
+    while(1) 
+    {
+        swapped = 0;
+        for(i=0;i<howMany-1;i++)
+        {
+                if(goals[i] > goals[i+1])
+                {
+                int temp = goals[i];
+                goals[i] = goals[i+1];
+                goals[i+1] = temp;
+                swapped=1;
+                }
+        }
+        if(swapped==0) {
+            break;
+        }
+        
+    }
+    
+    printf("\nSorted List\n");
+    for( i=0; i<howMany; i++)
+    {
+        printf("%d \n",goals[i]);
+    }
 
 return 0;
 }
