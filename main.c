ae6bf192152b446022194bc6c2dd868fe6a62fe7
Reuben Rankin <reubenrankin@hotmail.com>
2019-04-25 04:22:34 +0000

    Trying to create a menu (haven't tested fully)

--- main.c
+++ main.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 #include <math.h>
-int main() 
+int main()
+ 
+
+
 {   // Variable declaration is always first. //
         
-    int operation, key;
+    int i, operation, key;         
+    char ceasarCipher[600], letter;
     
     
     // First of all we are going to create a kind of menu that the user can select //
@@ -19,25 +23,47 @@ int main()
   
     switch(operation)
     {
+        
         // Case 0 is Encription //
-        case 0:
+        case 0:     
+                    printf("Enter a message you wish to encript:\n");
+                    gets(ceasarCipher);
+                    printf("Enter the key (i.e. shift amount of positions): ");
+                    scanf("%d", &key);
+                    
+                    for(i=0;ceasarcipher[i] != '\0'; ++i)
+                    {
+                    letter = ceasarcipher[i];
+            
+                        if(letter >= 'A' && letter <= 'Z')
+                        {
+                        letter = letter + key;
+               
+                            if(letter > 'Z')
+                            {
+                            letter = letter - 'Z' + 'A' - 1;   
+                            }
+                        
+                        ceasarcipher[i] = letter; 
+                
+                        }       
+        
+            
+                    }           
+        
         break;
+        
+        
+        
+        
         // Case 1 is Decription. //
         case 1: 
         break;
         // Case 2 is Frequency Analysis of how many times each letter occurs, useful to find the 'fingerprint' //
-        case 2:
+        case 2: 
         break;
         
-        
-        
-    
-        
-        
-        
-        
-        
-        
+       
         
     }
     
