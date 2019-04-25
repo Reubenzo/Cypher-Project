30e067fabfecbddc8325ceaeeeb224dba2482077
Reuben Rankin <reubenrankin@hotmail.com>
2019-04-25 02:39:22 +0000

    Lift off...

--- main.c
+++ main.c
@@ -1,46 +1,46 @@
 #include <stdio.h>
-
+#include <math.h>
 int main() 
-{
-   int i, temp, swapped;
-   int howMany = 10;
-   int goals[howMany];
-   
-   for(i=0;i<howMany;i++)
-   {
-       goals[i] = ( rand()%25 )+1;
-   }
+{   // Variable declaration is always first. //
+        
+    int operation, key;
     
-    printf("Original List\n");
-    for(i=0;i<howMany;i++)
-    {
-        printf("%d \n", goals[i]);
-    }
     
-    while(1) 
+    // First of all we are going to create a kind of menu that the user can select //
+    // which operation they would like to perform. First we need a message //
+    // to ask the user to enter a number for our 'Switch Case' to go off and excecute. //
+    // We then assign the entered value to a variable so it can be given to the switch case. //
+
+    
+    printf("Please enter the operation code for the task you wish to perform:\n0) Encription\n1) Decription\n2) Statistical Analysis\n");
+    scanf("%d", &operation);
+    
+    // The switch (below) will now recieve the code and go off to excecute the desired task. //
+  
+    switch(operation)
     {
-        swapped = 0;
-        for(i=0;i<howMany-1;i++)
-        {
-                if(goals[i] > goals[i+1])
-                {
-                int temp = goals[i];
-                goals[i] = goals[i+1];
-                goals[i+1] = temp;
-                swapped=1;
-                }
-        }
-        if(swapped==0) {
-            break;
-        }
+        // Case 0 is Encription //
+        case 0:
+        break;
+        // Case 1 is Decription. //
+        case 1: 
+        break;
+        // Case 2 is Frequency Analysis of how many times each letter occurs, useful to find the 'fingerprint' //
+        case 2:
+        break;
+        
+        
+        
+    
+        
+        
+        
+        
+        
+        
         
     }
     
-    printf("\nSorted List\n");
-    for( i=0; i<howMany; i++)
-    {
-        printf("%d \n",goals[i]);
-    }
 
 return 0;
 }
