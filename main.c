f501f9606c4229a1ebd34549d47852ee353b8b57
Reuben Rankin <reubenrankin@hotmail.com>
2019-04-25 06:30:57 +0000

    Struggling with the menu system - trying to make functions that return to switch cases (see if that works).

--- main.c
+++ main.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <math.h>
-int main()
- 
 
+
+int encription(int i, int key, char ceasarCipher[100], int letter);
+int main()
 
 {   // Variable declaration is always first. //
         
@@ -19,55 +20,44 @@ int main()
     printf("Please enter the operation code for the task you wish to perform:\n0) Encription\n1) Decription\n2) Statistical Analysis\n\n");
     scanf("%d", &operation);
     
-    // The switch (below) will now recieve the code and go off to excecute the desired task. //
-  
     switch(operation)
     {
-        
-        // Case 0 is Encription //
-        case 0:     
-                    printf("Enter a message you wish to encript:\n\n");
-                    fgets(ceasarCipher,100,stdin);
-                    printf("Enter the key (i.e. shift amount of positions):\n\n");
-                    scanf("%d", &key);
-                    
-                    for(i = 0;ceasarCipher[i] != '\0'; ++i)
-                    {
-                    letter = ceasarCipher[i];
-            
-                        if(letter >= 'A' && letter <= 'Z')
-                        {
-                        letter = letter + key;
-               
-                            if(letter > 'Z')
-                            {
-                            letter = letter - 'Z' + 'A' - 1;   
-                            }
-                        
-                    ceasarCipher[i] = letter; 
-                
-                        }       
-        
-            
-                    } 
-                    printf("Your encripted message: %s", ceasarCipher);
-        
-        break;
-        
-        
-        
-        
-        // Case 1 is Decription. //
-        case 1: 
-        break;
-        // Case 2 is Frequency Analysis of how many times each letter occurs, useful to find the 'fingerprint' //
-        case 2: 
-        break;
-        
+        case 0: // I WANT TO CALL THE FUNCTION ENCRIPTION   
        
-        
+    
+        printf("Your encripted message: %s", ceasarCipher); 
     }
     
-
 return 0;
+}
+
+
+
+
+int encription(int i, int key, char ceasarCipher[100], int letter)
+{
+    printf("Enter a message you wish to encript, in CAPITAL LETTERS:\n\n");
+    fgets(ceasarCipher,100,stdin);
+    printf("Enter the key (i.e. shift amount of positions):\n\n");
+    scanf("%d", &key);
+                    
+    for(i=0;ceasarCipher[i] != '\0'; ++i)
+    {
+    letter = ceasarCipher[i];
+            
+        if(letter >= 'A' && letter <= 'Z')
+        {
+        letter = letter + key;
+               
+            if(letter > 'Z')
+            {
+            letter = letter - 'Z' + 'A' - 1;   
+            }
+                        
+        ceasarCipher[i] = letter;        
+        }       
+    
+            
+    }
+    return (ceasarCipher);
 }
