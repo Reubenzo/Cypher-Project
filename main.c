b8cdfd2a83a78d8b483f0f03049b302f5929b1ac
Reuben Rankin <reubenrankin@hotmail.com>
2019-04-28 23:38:16 +0000

    Cannot understand whats wrong with input (Moving to codeboard.io) will still update in che for marking/git purposes

--- main.c
+++ main.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 #include <math.h>
 
-
-int encription(int i, int key, char ceasarCipher[100], int letter);
 int main()
 
 {   // Variable declaration is always first. //
         
-    int i, operation, key;         
+    int i, operation1, operation2, key;         
     char ceasarCipher[100], letter;
     
     
@@ -17,47 +15,86 @@ int main()
     // We then assign the entered value to a variable so it can be given to the switch case. //
 
     
-    printf("Please enter the operation code for the task you wish to perform:\n0) Encription\n1) Decription\n2) Statistical Analysis\n\n");
-    scanf("%d", &operation);
+    printf("Please enter the operation code for the task you wish to perform:\n0) Encription\n1) Decription\n2) Decription without a key\n\n");
+    scanf("%d", &operation1);
     
-    switch(operation)
+            char tmp;
+            scanf("%c",&tmp);
+    if(operation1 != 0||1||2)
     {
-        case 0: // I WANT TO CALL THE FUNCTION ENCRIPTION   
+        printf("ERROR: That is not a task code");
+    }
+    
+    else
+    {
+    
+    
+    switch(operation1){
+        
+        case 0:         printf("Please choose and enter key from the following options: \n");
+                        printf("[1]Rotation Cipher or [2]Substitution Cipher \n\n------------\n\n");
+                        scanf("%d", &operation2);
+        
+        if(operation2 == 1)
+        {
+                    printf("Enter a message you wish to encript, in CAPITAL LETTERS:\n\n");
+                    fgets(ceasarCipher,100,stdin);
+                    printf("Enter the key (i.e. shift amount of positions):\n\n");
+                    scanf("%d", &key);
+                    
+                    for(i=0;ceasarCipher[i] != '\0'; ++i)
+                    {
+                        letter = ceasarCipher[i];
+            
+                        if(letter >= 'A' && letter <= 'Z')
+                        {
+                            letter = letter + key;
+                        }
+
+                        if(letter > 'Z')
+                        {
+                            letter = letter - 'Z' + 'A' - 1;   
+                        }
+                        
+                    ceasarCipher[i] = letter;        
+                    }
+                    
+         printf("Your encripted message: %s", ceasarCipher); 
+                    
+         }
+         
+         
+        else if(operation2 == 2)
+     
+        {   
+        
+        //SUBSTITUTION ENCRIPTION CODE HERE  
+        
+        }
+    
+            else
+            {
+            printf("No input detected...");
+            }
+    
+         
+    break;
+         
+    
+    
+            
+                    }
        
     
-        printf("Your encripted message: %s", ceasarCipher); 
+       
+    }    
+
+        return 0;
     }
     
-return 0;
-}
 
 
 
 
-int encription(int i, int key, char ceasarCipher[100], int letter)
-{
-    printf("Enter a message you wish to encript, in CAPITAL LETTERS:\n\n");
-    fgets(ceasarCipher,100,stdin);
-    printf("Enter the key (i.e. shift amount of positions):\n\n");
-    scanf("%d", &key);
-                    
-    for(i=0;ceasarCipher[i] != '\0'; ++i)
-    {
-    letter = ceasarCipher[i];
-            
-        if(letter >= 'A' && letter <= 'Z')
-        {
-        letter = letter + key;
-               
-            if(letter > 'Z')
-            {
-            letter = letter - 'Z' + 'A' - 1;   
-            }
-                        
-        ceasarCipher[i] = letter;        
-        }       
     
-            
-    }
-    return (ceasarCipher);
-}
+
